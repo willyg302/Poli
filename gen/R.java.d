60858b0891a030fe2eb9b96c66a7c88d7d1e16c9
William Gaul <willyg302@gmail.com>
2013-09-03 14:21:26 -1000

    Parrot icon w00t!

--- gen/R.java.d
+++ gen/R.java.d
@@ -21,15 +21,4 @@ C:\Users\William Gaul\Documents\NetBeansProjects\Poli\res\values\dimens.xml \
 C:\Users\William Gaul\Documents\NetBeansProjects\Poli\res\values\ids.xml \
 C:\Users\William Gaul\Documents\NetBeansProjects\Poli\res\values\strings.xml \
 C:\Users\William Gaul\Documents\NetBeansProjects\Poli\res\values\styles.xml \
-C:\Users\William Gaul\Documents\NetBeansProjects\Poli\bin\res\drawable\compare.png \
-C:\Users\William Gaul\Documents\NetBeansProjects\Poli\bin\res\drawable\home.png \
-C:\Users\William Gaul\Documents\NetBeansProjects\Poli\bin\res\drawable\mock_vote.png \
-C:\Users\William Gaul\Documents\NetBeansProjects\Poli\bin\res\drawable\my_elections.png \
-C:\Users\William Gaul\Documents\NetBeansProjects\Poli\bin\res\drawable\search.png \
-C:\Users\William Gaul\Documents\NetBeansProjects\Poli\bin\res\drawable\stats.png \
-C:\Users\William Gaul\Documents\NetBeansProjects\Poli\bin\res\drawable\whos_who.png \
-C:\Users\William Gaul\Documents\NetBeansProjects\Poli\bin\res\drawable-hdpi\ic_launcher.png \
-C:\Users\William Gaul\Documents\NetBeansProjects\Poli\bin\res\drawable-ldpi\ic_launcher.png \
-C:\Users\William Gaul\Documents\NetBeansProjects\Poli\bin\res\drawable-mdpi\ic_launcher.png \
-C:\Users\William Gaul\Documents\NetBeansProjects\Poli\bin\res\drawable-xhdpi\ic_launcher.png \
 C:\Users\William Gaul\Documents\NetBeansProjects\Poli\bin\AndroidManifest.xml \
